ea3799831342df8eaa3e63a7f7abf046e9127fd9
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-08 20:58:44 +0100

    feat: fix export path

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""Defines a type-annotated function `make_multiplier` that takes
+"""
+Defines a type-annotated function `make_multiplier` that takes
 a float multiplier as argument and returns a function that
 multiplies a float by multiplier.
 """
-from collections.abc import Callable
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
