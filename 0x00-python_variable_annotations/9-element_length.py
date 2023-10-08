b1585cc70fac764e9dfcd9eb65e6f295d803f4de
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-08 21:18:23 +0100

    fix

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """stuff"""
 
-from typing import List
+from typing import List, Tuple
 
 
-def element_length(lst: List[str]):
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """split a string"""
     return [(i, len(i)) for i in lst]
