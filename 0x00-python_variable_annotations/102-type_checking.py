5d832a8464111928cd28ddf54634995969c44d65
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-09 13:09:59 +0100

    fix types

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,6 +4,7 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """fix types"""
     zoomed_in: List = [
         item for item in lst
         for _ in range(factor)
