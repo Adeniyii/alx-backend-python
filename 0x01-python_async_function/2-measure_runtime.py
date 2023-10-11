4f8ec0c7d366297b76095f47b311064116a795fa
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-11 17:12:31 +0100

    fix

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,20 +2,12 @@
 """waiting for youuu ..."""
 import time
 import asyncio
-# from typing import Any, Callable, Coroutine, List
-
-# wait_n: Callable[[int, int], Coroutine[Any, Any, List[float]]] = __import__(
-#     '1-concurrent_coroutines').wait_n
-
-wait_n = __import__(
-    '1-concurrent_coroutines').wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, delay: int) -> float:
     """measure time taken to run wait_n"""
-    past: float = time.time()
+    past = time.time()
     asyncio.run(wait_n(n, delay))
-    duration: float = time.time() - past
-    true_duration = duration / float(n)
 
-    return true_duration
+    return (time.time() - past) / n
