1df770fed4a84b10097ee4707033343bf444033f
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-11 16:34:58 +0100

    feat: benchmarking async function

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""waiting for youuu ..."""
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, delay: int) -> float:
+    """measure time taken to run wait_n"""
+    past = time.time()
+    asyncio.run(wait_n(n, delay))
+    duration = time.time() - past
+
+    return duration / n
+
+
+if __name__ == "__main__":
+    n = 5
+    max_delay = 9
+
+    print(measure_time(n, max_delay))
