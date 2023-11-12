f7ceadc089ce7540b0869957562f8175cffcb694
Adeniyii <ifedayoadeniyi@gmail.com>
2023-11-12 14:24:31 +0100

    feat: mock out request object

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,11 @@
 """"""
 from typing import Any, Mapping, Sequence
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
 
 access_nested_map = __import__("utils").access_nested_map
+utils = __import__("utils")
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -28,3 +30,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url: str, payload: Mapping):
+        """"""
+        with patch('utils.requests.get') as mock_get:
+            mock_get.return_value.json.return_value = payload
+            self.assertEqual(utils.get_json(url), payload)
+            mock_get.assert_called_once_with(url)
